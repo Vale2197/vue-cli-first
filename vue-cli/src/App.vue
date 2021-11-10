c5c647e7608346a1691d3163bb2229f0f42b3cdf
Vale2197 <valentinatrebbi97@gmail.com>
2021-11-10 16:15:25 +0100

    creata "hero image (componente)"

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,24 +1,30 @@
 <template>
   <div id="app">
     <Title/>
+    <hero/>
   </div>
 </template>
 
 <script>
 /* import HelloWorld from "./components/HelloWorld.vue"; */
 import Title from "./components/Title.vue";
+import hero from "./components/Hero.vue";
 
 export default {
   name: "App",
   /* components: { */
     /* HelloWorld, */
     components: {
-      Title
-    }
-  
+      Title,
+      hero
+    },
 };
 </script>
 
 <style lang="scss">
-
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
 </style>
---
+++ vue-cli/src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hero">
+        <div class="heroImg">
+            <img src="https://picsum.photos/700/400" alt="jj"> 
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "hero"
+    };
+</script>
+
+<style>
+    img {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    object-position: center;
+    }
+</style>
